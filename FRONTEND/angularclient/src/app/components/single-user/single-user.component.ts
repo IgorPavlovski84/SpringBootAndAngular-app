2d380e1d9d85391dc40a0b9628f67ecf8e452d86
IgorPavlovski84 <60329780+IgorPavlovski84@users.noreply.github.com>
2021-02-06 23:27:52 +0100

    created URL for a unique user /{id}

--- FRONTEND/angularclient/src/app/components/single-user/single-user.component.ts
+++ FRONTEND/angularclient/src/app/components/single-user/single-user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {User} from "../../models/user";
 import {UserService} from "../../services/user.service";
 import {ActivatedRoute} from "@angular/router";
@@ -14,35 +14,22 @@ export class SingleUserComponent implements OnInit {
 
   user!: User;
 
-  @Input() userId!: number;
-
   broj!: number;
 
   constructor(private userService: UserService,
               private route: ActivatedRoute
   ) {
-  //   this.userService.findUser(this.broj).subscribe(data => {
-  //     this.user = data;
-  //   });
+
     this.user = new User();
+    this.broj = Number(this.route.snapshot.params.id);
+    this.userService.findUser(this.broj).subscribe(data => {
+      this.user = data;
+      // console.log(this.user);
+    });
   }
 
   ngOnInit() {
-    debugger;
-    this.broj = Number(this.route.snapshot.params.id);
-    debugger;
-    this.userService.findUser(this.broj).subscribe(data => {
-      this.user = data;
-      console.log(this.user);
-    });
-    debugger;
   }
-
-  // showUser() {
-  //   this.userService.findUser(this.broj).subscribe(data => {
-  //     this.user = data;
-  //   });
-  // }
 
 }
 
